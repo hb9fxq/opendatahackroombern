75b4d2c75401aa4cb32c1e4d793c7904cb0984c4
Frank Werner-Krippendorf <wernerkrippendorf@kripp.ch>
2014-09-06 12:25:54 +0200

    test api

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home', name='home'),
+    url(r'^api$', 'views.apiSample', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
